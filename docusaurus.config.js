d371a7b964e537e4fac0dc86579b6357257d4bb0
DSalman03 <danny.f.salman@gmail.com>
2023-08-23 22:03:47 -0400

    add navbar config + edits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,4 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -9,83 +7,71 @@ const config = {
   title: 'Avail Docs',
   tagline: '',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://availproject.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'availproject', // Usually your GitHub org/user name.
-  projectName: 'availproject.github.io', // Usually your repo name.
+  organizationName: 'availproject',
+  projectName: 'availproject.github.io',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
-	  routeBasePath: '/', // Serve the docs at the site's root
-//          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-//          editUrl:
-//            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Avail Docs',
-        items: [
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Links',
-            items: [
-              {
-                label: 'Avail Home',
-                href: 'https://www.availproject.org',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Erasure Coding Technologies Ltd.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['rust'],
-      },
-    }),
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Avail Docs',
+      items: [
+        {
+          type: 'doc',
+          docId: 'introduction/introduction',
+          position: 'left',
+          label: 'About',
+        },
+        {
+          href: 'https://github.com/availproject',
+          position: 'right',
+          label: 'GitHub',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Links',
+          items: [
+            {
+              label: 'Avail Home',
+              href: 'https://www.availproject.org',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Erasure Coding Technologies Ltd.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      additionalLanguages: ['rust'],
+    },
+  },
 };
 
 module.exports = config;
